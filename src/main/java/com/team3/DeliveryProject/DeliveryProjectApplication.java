e81c57790f4f7d5c32d4b6d8412d8000f7e2c5c6
FrankSausage <85215937+FrankSausage@users.noreply.github.com>
2024-04-16 14:03:58 +0900

    Initial commit

---
+++ src/main/java/com/team3/DeliveryProject/DeliveryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.team3.DeliveryProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team3/DeliveryProject/DeliveryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team3.DeliveryProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
