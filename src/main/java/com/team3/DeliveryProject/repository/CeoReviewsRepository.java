d9363705bdee456e6b71742c2d2cfd149ae83c06
Kim Dong Ho <85215937+FrankSausage@users.noreply.github.com>
2024-04-19 12:50:23 +0900

    Revert "Sub main (#1)"
    This reverts commit 83e18b21e819c70d0b48deca2d75ae043dc412f2.

--- src/main/java/com/team3/DeliveryProject/repository/CeoReviewsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.team3.DeliveryProject.repository;
-
-import com.team3.DeliveryProject.entity.CeoReviews;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CeoReviewsRepository  extends JpaRepository<CeoReviews, Long> {
-}
--- src/main/java/com/team3/DeliveryProject/repository/MenuOptionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.team3.DeliveryProject.repository;
-
-import com.team3.DeliveryProject.entity.MenuOption;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MenuOptionRepository  extends JpaRepository<MenuOption, Long> {
-}
