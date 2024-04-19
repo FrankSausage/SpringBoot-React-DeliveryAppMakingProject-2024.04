afc403de654b5afd735649e30af88b32ce895760
Kim Dong Ho <85215937+FrankSausage@users.noreply.github.com>
2024-04-19 12:50:23 +0900

    Revert "Sub main (#1)"
    This reverts commit 83e18b21e819c70d0b48deca2d75ae043dc412f2.

--- src/main/java/com/team3/DeliveryProject/repository/DibsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.team3.DeliveryProject.repository;
-
-import com.team3.DeliveryProject.entity.Dibs;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DibsRepository  extends JpaRepository<Dibs, Long> {
-}
--- src/main/java/com/team3/DeliveryProject/repository/ReviewsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.team3.DeliveryProject.repository;
-
-import com.team3.DeliveryProject.entity.Reviews;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReviewsRepository  extends JpaRepository<Reviews, Long> {
-}
