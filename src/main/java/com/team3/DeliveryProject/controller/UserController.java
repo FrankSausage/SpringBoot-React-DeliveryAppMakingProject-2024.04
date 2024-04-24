645d4067a9f6078dc7410109cb5eaca810061fe2
kijun2423db <kijun2423db@naver.com>
2024-04-24 17:11:57 +0900

    [Feature] 유저정보수정 API Get메서드 개발

--- src/main/java/com/team3/DeliveryProject/controller/UserController.java
+++ src/main/java/com/team3/DeliveryProject/controller/UserController.java
@@ -1,16 +1,21 @@
 package com.team3.DeliveryProject.controller;
 
-import com.team3.DeliveryProject.dto.request.UserSignUpRequestDto;
-import com.team3.DeliveryProject.dto.request.UserUpdateRequestDto;
+import com.team3.DeliveryProject.dto.request.user.UserSignUpRequestDto;
+import com.team3.DeliveryProject.dto.request.user.UserUpdateGetRequestDto;
+import com.team3.DeliveryProject.dto.request.user.UserUpdatePostRequestDto;
+import com.team3.DeliveryProject.dto.response.user.UserUpdateResponseDto;
 import com.team3.DeliveryProject.entity.Users;
 import com.team3.DeliveryProject.repository.UsersRepository;
 import com.team3.DeliveryProject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user")
@@ -32,7 +37,7 @@ public class UserController {
     }
 
     @PostMapping("/update")
-    public ResponseEntity<?> updateUser(@RequestBody UserUpdateRequestDto requestDto){
+    public ResponseEntity<?> updateUser(@RequestBody UserUpdatePostRequestDto requestDto){
         System.out.println("컨트롤러 진입");
 
         Users user = usersRepository.findUsersByEmail(requestDto.getEmail()).get();
@@ -46,6 +51,20 @@ public class UserController {
         System.out.println(user);
         userService.updateUser(user);
         return ResponseEntity.ok().body("User update successfully");
+    }
+
+    @GetMapping("/update")
+    public UserUpdateResponseDto updateUser(@ModelAttribute UserUpdateGetRequestDto requestDto) {
+        System.out.println("컨트롤러 진입");
+        Users user = usersRepository.findUsersByEmail(requestDto.getEmail()).orElseThrow(() -> new RuntimeException("User not found"));
+        System.out.println(user);
+
+        UserUpdateResponseDto responseDto = UserUpdateResponseDto.builder()
+            .phone(user.getPhone())
+            .currentAddress(user.getCurrentAddress())
+            .build();
+        System.out.println(responseDto);
+        return responseDto;
     }
 
 }
