3bbb1f641f8970d19fe3fb5ee97f044fc135fbe2
kijun2423db <kijun2423db@naver.com>
2024-04-24 16:01:21 +0900

    [Feature] Address 서비스 개발

--- src/main/java/com/team3/DeliveryProject/responseCode/ResponseCode.java
+++ src/main/java/com/team3/DeliveryProject/responseCode/ResponseCode.java
@@ -8,6 +8,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ResponseCode {
     USER_SIGNUP_SUCCESS(HttpStatus.OK, "회원가입 성공"),
+    ADDRESS_ADD_SUCCESS(HttpStatus.OK,"주소 추가 성공"),
+    ADDRESS_MODIFY_SUCCESS(HttpStatus.OK,"주소 수정 성공"),
+    ADDRESS_DELETE_SUCCESS(HttpStatus.OK,"주소 삭제 성공"),
     USER_INFO_UPDATE_SUCCESS(HttpStatus.OK, "계정정보 변경 성공"),
     ACCESS_TOKEN_IS_VALID(HttpStatus.OK, "유효한 토큰입니다"),
     COMPANY_CREATE_SUCCESS(HttpStatus.OK, "회사정보 생성 성공"),
