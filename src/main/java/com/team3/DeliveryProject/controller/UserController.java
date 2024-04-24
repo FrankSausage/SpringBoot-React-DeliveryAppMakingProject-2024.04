a8a4736238b3e44d21dec5f9e45b5a051804282a
kijun2423db <kijun2423db@naver.com>
2024-04-24 16:01:40 +0900

    [Fix] User 서비스 로직 수정 및 필요없는 코드 삭제

--- src/main/java/com/team3/DeliveryProject/controller/UserController.java
+++ src/main/java/com/team3/DeliveryProject/controller/UserController.java
@@ -25,24 +25,10 @@ public class UserController {
         System.out.println("컨트롤러 진입");
 
         Users users = new Users(userRequestDto.getPassword(),userRequestDto.getName(),userRequestDto.getPhone(),
-            userRequestDto.getEmail(),0,userRequestDto.getRole(),userRequestDto.getCurrentAddress(),"일반",0);
+            userRequestDto.getEmail(),0,userRequestDto.getRole(),userRequestDto.getCurrentAddress(),"우리집",0);
         System.out.println(users);
         System.out.println("!@@!@#W@!!@#@!#@!#@!#!@#");
         userService.signUp(users);
         return ResponseEntity.ok().body("User registered successfully");
     }
-
-//    @PostMapping("/signup")
-//    public ResponseEntity<?> signUp(String userId, String password, String name, String phone, String email, String role, String currentAddress) {
-//        System.out.println("컨트롤러 진입");
-//
-//        System.out.println(userId);
-//        System.out.println(password);
-//        System.out.println(name);
-//        System.out.println(phone);
-//        System.out.println(email);
-//        System.out.println(role);
-//        System.out.println(currentAddress);
-//        return ResponseEntity.ok().body("User registered successfully");
-//    }
 }
--- src/main/java/com/team3/DeliveryProject/service/UserServiceImpl.java
+++ src/main/java/com/team3/DeliveryProject/service/UserServiceImpl.java
@@ -25,11 +25,9 @@ public class UserServiceImpl implements UserService{
     public ResponseEntity<Response> signUp(Users user) {
         if (usersRepository.findUsersByUserId(user.getUserId()).isPresent()){
             System.out.println("회원가입 오류 (UserId가 중복되었습니다.)");
-            Response response = new Response(400,"회원가입 오류 (UserId가 중복되었습니다.");
             return Response.toResponseEntity(USERNAME_IS_ALREADY_EXIST);
         } else if (usersRepository.findUsersByEmail(user.getEmail()).isPresent()) {
             System.out.println("회원가입 오류 (Email이 중복되었습니다.)");
-            Response response = new Response(400, "회원가입 오류 (Email이 중복되었습니다.)");
             return Response.toResponseEntity(USER_EMAIL_IS_ALREADY_EXIST);
         }else{
             System.out.println("서비스 정상로직 진입");
