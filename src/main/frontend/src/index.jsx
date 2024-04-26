1a5ef33992b25b03185d36540c869d136aa099e1
FrankSausage <85215937+FrankSausage@users.noreply.github.com>
2024-04-26 18:18:47 +0900

    [Feature] 유저 정보 업데이트 구현

--- src/main/frontend/src/index.jsx
+++ src/main/frontend/src/index.jsx
@@ -8,6 +8,7 @@ import SignUp from './pages/SignUp';
 import Home from './pages/Home';
 import Update from './pages/Update';
 import ProtectedRoute from './pages/ProtectedRoute';
+import TestField from './pages/TestField';
 
 const router = createBrowserRouter([
   {
@@ -17,6 +18,7 @@ const router = createBrowserRouter([
       { index: true, element: <Home /> },
       { path: 'Signin', element: <SignIn />},
       { path: 'SignUp', element: <SignUp />},
+      { path: 'test', element: <TestField />},
       { path: 'Update', 
       element: <ProtectedRoute><Update /></ProtectedRoute>},
     ]
