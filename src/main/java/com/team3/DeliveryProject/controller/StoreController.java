de86c65b0e6fa98fcf37f560433f5397db82734b
kijun2423db <kijun2423db@naver.com>
2024-04-26 17:12:20 +0900

    [Feature] 가게 수정 API 개발

--- src/main/java/com/team3/DeliveryProject/controller/StoreController.java
+++ src/main/java/com/team3/DeliveryProject/controller/StoreController.java
@@ -1,6 +1,7 @@
 package com.team3.DeliveryProject.controller;
 
 import com.team3.DeliveryProject.dto.request.store.StoreAddRequestDto;
+import com.team3.DeliveryProject.dto.request.store.StoreUpdateRequestDto;
 import com.team3.DeliveryProject.dto.request.user.UserSignUpRequestDto;
 import com.team3.DeliveryProject.entity.Stores;
 import com.team3.DeliveryProject.repository.StoresRepository;
@@ -10,6 +11,7 @@ import com.team3.DeliveryProject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +19,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/store")
 public class StoreController {
+
     @Autowired
     private StoreService storeService;
+
     @PostMapping("/register")
     public ResponseEntity<?> addStore(@RequestBody StoreAddRequestDto requestDto) {
         storeService.addStore(requestDto);
         return ResponseEntity.ok().body("Store add successfully");
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<?> updateStore(@RequestBody StoreUpdateRequestDto requestDto) {
+        storeService.updateStore(requestDto);
+        return ResponseEntity.ok().body("Store update successfully");
     }
 }
--- src/main/java/com/team3/DeliveryProject/service/StoreService.java
+++ src/main/java/com/team3/DeliveryProject/service/StoreService.java
@@ -2,10 +2,13 @@ package com.team3.DeliveryProject.service;
 
 import com.team3.DeliveryProject.dto.common.Response;
 import com.team3.DeliveryProject.dto.request.store.StoreAddRequestDto;
+import com.team3.DeliveryProject.dto.request.store.StoreUpdateRequestDto;
 import com.team3.DeliveryProject.entity.Stores;
 import org.springframework.http.ResponseEntity;
 
 public interface StoreService {
 
     public ResponseEntity<Response> addStore(StoreAddRequestDto requestDto);
+
+    public ResponseEntity<Response> updateStore(StoreUpdateRequestDto requestDto);
 }
