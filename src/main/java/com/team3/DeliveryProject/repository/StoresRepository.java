fbdd05facd7b24efe6a8850bda2fc2d1caf72ebe
kijun2423db <kijun2423db@naver.com>
2024-04-30 12:57:09 +0900

    [Feature] 가게 JPA 메서드 추가

--- src/main/java/com/team3/DeliveryProject/repository/StoresRepository.java
+++ src/main/java/com/team3/DeliveryProject/repository/StoresRepository.java
@@ -1,9 +1,13 @@
 package com.team3.DeliveryProject.repository;
 
+import com.team3.DeliveryProject.entity.Address;
 import com.team3.DeliveryProject.entity.Stores;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StoresRepository  extends JpaRepository<Stores, Long> {
+    List<Stores> findAllByUserId(Long userId);
+
 }
