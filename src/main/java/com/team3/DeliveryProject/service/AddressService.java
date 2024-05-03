058b0c6a560d9d34df1d92f5d78a47b4037053ab
kijun2423db <kijun2423db@naver.com>
2024-05-03 17:16:34 +0900

    [Feature] 주소 변경 관련 컨트롤러, 서비스 개발

--- src/main/java/com/team3/DeliveryProject/service/AddressService.java
+++ src/main/java/com/team3/DeliveryProject/service/AddressService.java
@@ -2,6 +2,7 @@ package com.team3.DeliveryProject.service;
 
 import com.team3.DeliveryProject.dto.common.Response;
 import com.team3.DeliveryProject.dto.request.address.AddressAddRequestDto;
+import com.team3.DeliveryProject.dto.request.address.AddressChangeRequestDto;
 import com.team3.DeliveryProject.dto.request.address.AddressDeleteRequestDto;
 import com.team3.DeliveryProject.dto.request.address.AddressFindAllRequestDto;
 import com.team3.DeliveryProject.dto.request.address.AddressModifyRequestDto;
@@ -15,5 +16,5 @@ public interface AddressService {
     public ResponseEntity<Response> modifyAddress(AddressModifyRequestDto requestDto);
     public ResponseEntity<Response> deleteAddress(AddressDeleteRequestDto requestDto);
     public List<AddressFindAllResponseDto> findAllAddress(AddressFindAllRequestDto requestDto);
-
+    public ResponseEntity<Response> changeCurrentAddress(AddressChangeRequestDto requestDto);
 }
