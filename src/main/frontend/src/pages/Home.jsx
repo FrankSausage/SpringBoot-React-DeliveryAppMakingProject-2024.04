edee5d5c1708b0a3449ce762f5cd35c52b6480f8
GimGiju <tigerwonder@naver.com>
2024-05-07 12:19:43 +0900

    Update Home.jsx

--- src/main/frontend/src/pages/Home.jsx
+++ src/main/frontend/src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import { Box } from "@mui/material";
-import SearchHeader from "../components/SearchHeader"
 import Footer from "../components/Footer"
 import UserMain from "../components/UserMain";
 import OwnerMain from "../components/OwnerMain";
@@ -7,7 +6,6 @@ import OwnerMain from "../components/OwnerMain";
 //주소: http://localhost:3000
 
 export default function Home() {
-    // const role = '회원'
     const role = localStorage.getItem('role') ? localStorage.setItem('role') : '회원';
     return(
         <Box sx={{ margin: -1 }}>
