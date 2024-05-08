0d7dc7bd5b5edf137aec37ffaa3b2c320067a852
kijun2423db <kijun2423db@naver.com>
2024-05-08 10:09:05 +0900

    [Feature] 점주 로그인 시 storeId반환 로직 추가

--- src/main/java/com/team3/DeliveryProject/dto/response/user/UserSignInRoleOwnerInnerDto.java
+++ src/main/java/com/team3/DeliveryProject/dto/response/user/UserSignInRoleOwnerInnerDto.java
@@ -14,6 +14,7 @@ import lombok.ToString;
 @AllArgsConstructor
 @Builder
 public class UserSignInRoleOwnerInnerDto {
+    private Long storeId;
     private String storePictureName;
     private String name;
 }
