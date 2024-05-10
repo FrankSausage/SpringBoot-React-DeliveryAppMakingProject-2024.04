ef29202b3d9737162cfc0a8d071dbff32219f9fd
GimGiju <tigerwonder@naver.com>
2024-05-10 20:27:58 +0900

    [Fix] MenuUpdate
    음식 정보 표시되게 만들었지만 아직 수정은 불가능합니다

--- src/main/frontend/src/pages/Stores/StoreMenuList.jsx
+++ src/main/frontend/src/pages/Stores/StoreMenuList.jsx
@@ -23,10 +23,6 @@ export default function StoreMenuList() {
     }
   }, [storeId]);
 
-  const handleLinkClick = () => {
-    navigate(`/MenuUpdate/${menuId}`);
-    console.log(menuId)
-  };
 
   useEffect(() => {
     if (menuData) {
@@ -37,7 +33,7 @@ export default function StoreMenuList() {
       }
     }
   }, [menuData, status]);
-  
+
   const handleCheckboxChange = async (index) => {
     const newStatuses = [...status];
     newStatuses[index] = !newStatuses[index];
@@ -47,14 +43,14 @@ export default function StoreMenuList() {
       const { email } = getCurrentUser();
       const menuId = menuData.categories
         .flatMap(category => category.menus)
-        [index].menuId;
-  
+      [index].menuId;
+
       const response = await axios.post(`/dp/store/menu/status`, {
         menuId: menuId,
         email: email,
         status: newStatuses[index] ? '품절' : '일반'
       });
-  
+
       console.log(response.data);
     } catch (error) {
       console.error('에러 발생:', error);
@@ -74,20 +70,22 @@ export default function StoreMenuList() {
                 <Grid container sx={{ position: 'relative', border: 1, borderColor: 'rgba(255, 0, 0, 0)', justifyContent: 'center', alignItems: 'center' }}>
                   <Grid className="centerBody" container columnSpacing={{ xs: 2, sm: 2 }} sx={gridStyle}>
                     <Box key={res.menuId} sx={{ ...boxStyle, position: 'relative', width: { xs: '90%', sm: '47%' }, height: '120px', marginX: 'auto' }}>
-                      <div onClick={handleLinkClick} style={{ textDecoration: 'none', color: 'black', cursor: 'pointer' }}>
-                        <img src={res.menuPictureName} style={{ width: '20%', height: '100%', position: 'absolute', top: 0, left: 0 }} />
-                        <ul style={{ position: 'absolute', top: '50%', left: '40%', transform: 'translate(-50%, -50%)', padding: 0, margin: 0 }}>
-                          <li style={{ listStyleType: 'none' }}>{res.name}</li>
-                          <li style={{ listStyleType: 'none' }}>인기 : {res.popularity} </li>
-                          <li style={{ listStyleType: 'none' }}>구성 : {res.content} </li>
-                          {status[idx]  && (
-                            <li style={{ listStyleType: 'none' }}>{res.status}</li>
-                          )}
-                        </ul>
-                      </div>
+                      <Link to={`/MenuUpdate/${res.menuId}`} state={{ menuId: res.menuId, storeId: storeId }} style={{ textDecoration: 'none', color: 'black' }} >
+                        <div style={{ textDecoration: 'none', color: 'black', cursor: 'pointer' }}>
+                          <img src={res.menuPictureName} style={{ width: '20%', height: '100%', position: 'absolute', top: 0, left: 0 }} />
+                          <ul style={{ position: 'absolute', top: '50%', left: '40%', transform: 'translate(-50%, -50%)', padding: 0, margin: 0 }}>
+                            <li style={{ listStyleType: 'none' }}>{res.name}</li>
+                            <li style={{ listStyleType: 'none' }}>인기 : {res.popularity} </li>
+                            <li style={{ listStyleType: 'none' }}>구성 : {res.content} </li>
+                            {status[idx] && (
+                              <li style={{ listStyleType: 'none' }}>{res.status}</li>
+                            )}
+                          </ul>
+                        </div>
+                      </Link>
                       {role === '회원' ? null : (
                         <Grid container spacing={3} >
-                          <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'flex-end', mt: 5  }}>
+                          <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'flex-end', mt: 5 }}>
                             <Button
                               variant="contained"
                               color={status[idx] ? 'primary' : 'secondary'}
@@ -113,7 +111,7 @@ export default function StoreMenuList() {
             type="submit"
             variant="contained"
             style={{ textDecoration: 'none', color: 'white' }}
-            sx={{ mt: 3, mb: 10, width: '200px', height: '50px', fontSize: '1.2rem',  }}>
+            sx={{ mt: 3, mb: 10, width: '200px', height: '50px', fontSize: '1.2rem', }}>
             <Link Link to={`/MenuRegister/${storeId}`} state={{ storeId: storeId }} style={{ textDecoration: 'none', color: 'white' }}>메뉴 추가하기</Link>
           </Button>
         </div>
