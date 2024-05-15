1f231689b0c448b579c7e89d64952c0fc26496b9
FrankSausage <85215937+FrankSausage@users.noreply.github.com>
2024-05-16 07:17:35 +0900

    [Feature] 장바구니 기능 구현

---
+++ src/main/frontend/src/pages/Cart/Hook/useCart.jsx
@@ -0,0 +1,43 @@
+import { useMutation } from "@tanstack/react-query";
+
+export function useCart () {
+
+  const addItemToCart = async (itemData) => {
+    try {
+      if(!localStorage.getItem('cartItems')){
+        localStorage.setItem('cartItems', JSON.stringify([{...itemData.menus, ['quantity'] : 1, ['sequence']: 1}]))
+      } else {
+        let item = JSON.parse(localStorage.getItem('cartItems'));
+        item.push({...itemData.menus, ['quantity'] : 1, ['sequence']: (1 + item.length)});
+        return await localStorage.setItem('cartItems', JSON.stringify(item))
+      }
+    } catch (error) {
+      alert('장바구니 담기에 실패했습니다!')
+      console.log(error)
+    }
+  }
+
+  const minusItemQuantity = () => {
+    
+  }
+
+  const getPrice = async () => {
+    try {
+      let items = JSON.parse(localStorage.getItem('cartItems'));
+      let totalPrice = 0
+      
+      items.map((menuItems) => {
+        totalPrice += menuItems.menuPrice;
+        menuItems.menuOptions.map((optionItems) =>{
+          totalPrice += optionItems.price;
+        })
+      })
+
+      return await totalPrice;
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return { addItemToCart, getPrice }
+}
--- src/main/frontend/src/pages/Cart/View/Cart.jsx
+++ src/main/frontend/src/pages/Cart/View/Cart.jsx
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Box, Card, CardContent, Divider, Grid, Stack, Typography } from '@mui/material';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -7,20 +8,45 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Slide from '@mui/material/Slide';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { Box, Divider, Typography } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useCart } from '../Hook/useCart';
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
 export default function Cart({ allClose }) {
-  const [open, setOpen] = React.useState(false);
-  const handleClickOpen = () => setOpen(true);
+  const [ open, setOpen ] = React.useState(false);
+  const cartItems = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
+  const { getPrice } = useCart();
+  const [ totalPrice, setTotalPrice ] = useState(0);
+
+  useEffect(() =>{
+    if(cartItems) {
+      getPrice().then(res=> setTotalPrice(res))
+    }
+  }, [allClose])
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  }
   const handleClose = () => {
     allClose();
     setOpen(false)
   };
 
+  const handleDelte = () => {
+    console.log(getPrice())
+  } 
+
+  const handleMinus = () => {
+
+  }
+
+  const handlePlus = () => {
+
+  }
+  
   return (
     <React.Fragment>
       <React.Fragment>
@@ -36,17 +62,47 @@ export default function Cart({ allClose }) {
         onClose={handleClose}
         aria-describedby="alert-dialog-slide-description"
       >
-        <DialogTitle variant='h4' sx={{textAlign:'center'}}>{"장바구니"}</DialogTitle>
-		<Divider />
-        <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">
-            Let Google help apps determine location. This means sending anonymous
-            location data to Google, even when no apps are running.
+        <DialogTitle variant='h4' sx={{textAlign:'center', width:500}}>{"장바구니"}</DialogTitle>
+        <Typography sx={{textAlign:'center', mb:1}}>최종 가격: {totalPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원</Typography>
+        <DialogContent sx={{borderTop:1}}>
+          <DialogContentText id="주문 목록">
+            {cartItems &&
+              cartItems.map((menuItems) => (
+                <Card key={menuItems.sequence}>
+                  <CardContent sx={{mb:1, border:1, borderRadius:'1%'}}>
+                    <Typography variant='h5' sx={{mb:1, textAlign:'center'}}>{menuItems.menuName}</Typography>
+                    <Divider sx={{mb:2}}/>
+                    <Typography sx={{textAlign:'center'}}>기본 가격 : {menuItems.menuPrice}</Typography>
+                      {menuItems.menuOptions && menuItems.menuOptions.map((optionItems) => (
+                        <Stack direction={'row'} key={optionItems.menuOptionId}>
+                          <Grid container sx={{justifyContent:'start'}}>
+                            <Grid item xs={1}>
+                            </Grid>
+                            <Grid item xs={8}>
+                              <Typography>{optionItems.options}</Typography>
+                            </Grid>
+                            <Grid item xs={3} sx={{alignContent:'center', textAlign:'end'}}>
+                              <Typography>+{optionItems.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원</Typography>
+                            </Grid>
+                          </Grid>
+                        </Stack>
+                      ))}
+        <Divider sx={{my:2}}/>
+                    <Stack direction={'row'} sx={{justifyContent:'center'}}>
+                      <Button sx={{ml:-8}} onClick={handleDelte}><DeleteIcon color='error'/></Button>
+                      <Button sx={{border:1}} color='error' >-</Button>
+                      <Typography sx={{textAlign:'center', alignContent:'center', mx: 2}}>수량 : {menuItems.quantity}</Typography>
+                      <Button sx={{border:1}} >+</Button>
+                    </Stack>
+                  </CardContent>
+                </Card>
+              ))
+            }
           </DialogContentText>
         </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Disagree</Button>
-          <Button onClick={handleClose}>Agree</Button>
+        <DialogActions sx={{justifyContent:'space-around', mb:1}}>
+          <Button onClick={handleClose} color='error'>닫기</Button>
+          <Button onClick={handleClose}>주문하기</Button>
         </DialogActions>
       </Dialog>
     </React.Fragment>
