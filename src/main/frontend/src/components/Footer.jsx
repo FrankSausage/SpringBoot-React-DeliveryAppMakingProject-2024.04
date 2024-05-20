27323373bad1a004f1eede00bf26bc7a2407b52c
GimGiju <tigerwonder@naver.com>
2024-05-20 20:37:21 +0900

    [Fix] Footer

--- src/main/frontend/src/components/Footer.jsx
+++ src/main/frontend/src/components/Footer.jsx
@@ -7,7 +7,7 @@ export default function footer(props) {
       <Typography variant="body2" color="text.secondary" align="center" {...props}>
         {'Copyright © '}
         <Link color="inherit" href="https://mui.com/">
-          HumanD
+        경기도 수원시 팔달구 중부대로 100, 3층 T.031-239-5855 | 개인정보담당자 :  privacy@dp.co.kr|제휴문의 : partnership@dp.co.kr|고객만족센터 : support@ydp.co.kr
         </Link>{' '}
         {new Date().getFullYear()}
         {'.' + '04'}
