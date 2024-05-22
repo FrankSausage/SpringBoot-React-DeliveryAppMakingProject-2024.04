e01257e759344c266abc6deb9f22d4450bdf28e7
kijun2423db <kijun2423db@naver.com>
2024-05-22 14:31:53 +0900

    [Featrue] 리뷰 등록 개발

--- src/main/java/com/team3/DeliveryProject/service/ReviewServiceImpl.java
+++ src/main/java/com/team3/DeliveryProject/service/ReviewServiceImpl.java
@@ -1,7 +1,14 @@
 package com.team3.DeliveryProject.service;
 
+import static com.team3.DeliveryProject.responseCode.ResponseCode.REVIEW_ADD_SUCCESS;
+
 import com.team3.DeliveryProject.dto.common.Response;
 import com.team3.DeliveryProject.dto.request.review.ReviewAddRequestDto;
+import com.team3.DeliveryProject.entity.Reviews;
+import com.team3.DeliveryProject.entity.Users;
+import com.team3.DeliveryProject.repository.ReviewsRepository;
+import com.team3.DeliveryProject.repository.UsersRepository;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,8 +17,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class ReviewServiceImpl implements ReviewService{
 
+    private final UsersRepository usersRepository;
+    private final ReviewsRepository reviewsRepository;
+
     @Override
     public ResponseEntity<Response> addReview(ReviewAddRequestDto requestDto) {
-        return null;
+        Users users = usersRepository.findUsersByEmail(requestDto.getEmail()).orElseThrow(()->new RuntimeException("User not found"));
+        Reviews reviews = new Reviews(users.getUserId(), requestDto.getOrderId(), requestDto.getRating(),
+            requestDto.getContent(), requestDto.getReviewPictureName(), LocalDateTime.now());
+        reviewsRepository.save(reviews);
+        return Response.toResponseEntity(REVIEW_ADD_SUCCESS);
     }
 }
