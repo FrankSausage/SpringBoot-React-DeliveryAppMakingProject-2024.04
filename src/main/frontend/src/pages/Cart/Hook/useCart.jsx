f2c7df68490d2722a899e92814b94a07625854f7
FrankSausage <85215937+FrankSausage@users.noreply.github.com>
2024-05-28 18:56:50 +0900

    [Fix] 장바구니 중복처리 로직 보강

--- src/main/frontend/src/pages/Cart/Hook/useCart.jsx
+++ src/main/frontend/src/pages/Cart/Hook/useCart.jsx
@@ -7,23 +7,26 @@ export function useCart () {
       } else {
         let item = JSON.parse(localStorage.getItem('cartItems'));
 
+        // 카트 중복 처리 로직
         let isInside = false;
         let index = 0;
         for(let i = 0; i < item.length; i++) {
-          if(itemData.menus.menuName===item[i].menuName && itemData.menus.menuOptions.length===0 && item[i].menuOptions.length===0) {
+          if(itemData.menus.menuName===item[i].menuName && itemData.menus.menuOptions.length===0 && item[i].menuOptions.length===0) { // 메뉴 이름이 일치하고, 옵션이 없는 경우
             return await plusItemQuantity(i);
           }
 
-          if(itemData.menus.menuName===item[i].menuName && itemData.menus.menuOptions.length===item[i].menuOptions.length) {
-            if(itemData.menus.menuOptions[0].options===item[i].menuOptions[0].options) {
-              isInside = true;
-              index = i;
-              break;
+          if(itemData.menus.menuName===item[i].menuName && itemData.menus.menuOptions.length===item[i].menuOptions.length) { // 메뉴 이름이 일치하고, 옵션이 있으며, 옵션 갯수가 동일한 경우
+            for(let x = 0; x < item.menuOptions.length; x++) {
+              if(itemData.menus.menuOptions[0].options===item[i].menuOptions[x].options) { // 추가될 메뉴 옵션이 포함되어 있는지 확인
+                isInside = true;
+                index = i;
+                break;
+              }
             }
           }
         }
-
-        if(isInside===true) {
+        // 포함 되어 있을 시 완벽히 일치하는지 체크 로직
+        if(isInside===true) { 
           let count = 0;
           for(let i = 0; i < item[index].menuOptions.length; i++) {
             for(let x = 0; x < item[index].menuOptions.length; x++) {
@@ -42,7 +45,7 @@ export function useCart () {
           item.push({...itemData.menus, ['quantity'] : 1, ['sequence']: (1 + item.length)});
           return await localStorage.setItem('cartItems', JSON.stringify(item))
         } else {
-          if(window.confirm('장바구니에 이미 등록한 가게와 다른 가게입니다, 장바구니를 비우고 새로 주문하시겠습니까?')) {
+          if(window.confirm('다른 가게의 주문이 이미 장바구니에 있습니다, 장바구니를 비우고 이 가게에서 새로 주문하시겠습니까?')) {
             return await localStorage.setItem('cartItems', JSON.stringify([{...itemData.menus, ['quantity'] : 1, ['sequence']: 1}]))
           }
         }
