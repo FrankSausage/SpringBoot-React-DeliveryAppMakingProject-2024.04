3b35fe779762af37fa42d468bba1a9b36c9d28e2
YooHeej <sprite1139@gmail.com>
2024-05-31 05:04:00 +0900

    [Fix] MenuDetail Css
    메뉴 주문창 수정

--- src/main/frontend/src/pages/Stores/Menus/MenuDetail.jsx
+++ src/main/frontend/src/pages/Stores/Menus/MenuDetail.jsx
@@ -12,6 +12,8 @@ export default function MenuDetail() {
   const { addItemToCart } = useCart();
   const [ items, setItems ] = useState([]);
 
+  
+
   const handleAddItem = data => {
     if (!items.find(e => e === data)) {
       const newItems = [...items, data];
@@ -23,7 +25,7 @@ export default function MenuDetail() {
   }
 
   const handleSubmit = () => {
-    addItemToCart({menus: {menuId: menuId, menuName: menuDetailData.menus.name, 
+    addItemToCart({menus: {menuId: menuId, menuName: menuDetailData.menus.name, menuPictureName: menuDetailData.menus.menuPictureName, menuContent: menuDetailData.menus.content,
       menuPrice: menuDetailData.menus.price, storeId: storeId, storeName: storeName, menuOptions: items}})
     .then(res => {
       if(res===false){
@@ -42,49 +44,73 @@ export default function MenuDetail() {
         {!isLoading && menuDetailData && 
             <Grid container sx={{backgroundColor: 'rgba(255, 255, 255, 0.6)', mt:10 }}>
               <Grid item xs/>
-              <Grid item xs={4} sx={{ml: '70px', mr: 2}}>
-                <Card sx={{my:1}}>
-                  <CardContent sx={{m:1}}>
-                    <Typography variant="h6">가격</Typography>
-                    <Stack direction={'row'}>
-                      <Grid container sx={{justifyContent:'start'}}>
-                        <Grid item xs={5}>
-                          <FormControlLabel 
-                          control={<Radio defaultChecked/>}
-                          label={menuDetailData.menus.name}
-                          labelPlacement="end"
-                          />
-                        </Grid>
-                        <Grid item xs={4}/>
-                        <Grid item xs={3}sx={{alignContent:'center', textAlign:'end'}}>
-                          <Typography>{menuDetailData.menus.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원</Typography>
-                        </Grid>
-                      </Grid>
-                    </Stack>
-                  </CardContent>
-                </Card>
-                <Card sx={{mb:1}}>
-                  <CardContent sx={{m:1}}>
-                    <Typography variant="h6">옵션</Typography>
-                      {menuDetailData.menus.options && menuDetailData.menus.options.map((data, idx) => (
-                        <Stack direction={'row'} key={idx}>
+              <Grid item xs={6} sx={{ml: '70px', mr: 2}}>
+                <Grid container spacing={2}>
+                 <Grid item xs={6}>
+                  <Card sx={{ my:1, height: '95%' }}>
+                    <CardContent sx={{m:1, height: '100%' }}>
+                      <Stack direction={'row'} alignItems="center" sx={{ height: '100%' }}>
+                      <Grid container sx={{justifyContent:'start', height: '100%' }}>
+                          <Grid item xs={5} sx={{ display: 'flex', alignItems: 'center', justifyContent:'center'}}>
+                            {menuDetailData.menus.menuPictureName ? (<img src={menuDetailData.menus.menuPictureName} alt={menuDetailData.menus.name} style={{ width: '100%', height: '60%', objectFit: 'cover' }} />
+                            ) : (
+                              <Typography>이미지를 불러올 수 없습니다.</Typography>
+                            )}
+                          </Grid>
+                          <Grid item xs={2}/>
+                            <Grid item xs={4}sx={{alignContent:'center', textAlign:'end'}}>
+                              <Typography variant="h6">{menuDetailData.menus.content}</Typography>
+                            </Grid>
+                          </Grid>
+                      </Stack>
+                    </CardContent>
+                  </Card>
+                </Grid>
+                  <Grid item xs={6}>
+                    <Card sx={{my:1}}>
+                      <CardContent sx={{m:1}}>
+                        <Typography variant="h6">가격</Typography>
+                        <Stack direction={'row'} alignItems="center">
                           <Grid container sx={{justifyContent:'start'}}>
                             <Grid item xs={5}>
                               <FormControlLabel 
-                              control={<Checkbox onClick={() => handleAddItem(data)}/>}
-                              label={data.options}
+                              control={<Radio defaultChecked/>}
+                              label={menuDetailData.menus.name}
                               labelPlacement="end"
                               />
                             </Grid>
                             <Grid item xs={4}/>
-                            <Grid item xs={3} sx={{alignContent:'center', textAlign:'end'}}>
-                              <Typography>+{data.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원</Typography>
+                            <Grid item xs={3}sx={{alignContent:'center', textAlign:'end'}}>
+                              <Typography>{menuDetailData.menus.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원</Typography>
                             </Grid>
                           </Grid>
                         </Stack>
-                      ))}
-                  </CardContent>
-                </Card>
+                      </CardContent>
+                    </Card>
+                  <Card sx={{my:1}}>
+                    <CardContent sx={{m:1}}>
+                      <Typography variant="h6">옵션</Typography>
+                        {menuDetailData.menus.options && menuDetailData.menus.options.map((data, idx) => (
+                          <Stack direction={'row'} key={idx}>
+                            <Grid container sx={{justifyContent:'start'}}>
+                              <Grid item xs={5}>
+                                <FormControlLabel 
+                                control={<Checkbox onClick={() => handleAddItem(data)}/>}
+                                label={data.options}
+                                labelPlacement="end"
+                                />
+                              </Grid>
+                              <Grid item xs={4}/>
+                              <Grid item xs={3} sx={{alignContent:'center', textAlign:'end'}}>
+                                <Typography>+{data.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원</Typography>
+                              </Grid>
+                            </Grid>
+                          </Stack>
+                        ))}
+                    </CardContent>
+                  </Card>
+                  </Grid>
+                </Grid>
               </Grid>
               <Grid item xs={1}>
                 <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
